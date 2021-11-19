a613f24aa0cb5f9c059923182025a69db84ab43a
Jess Varghese <jessvarghese@gmail.com>
2021-11-19 12:37:27 -0500

    finished site

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template')
-
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
 
@@ -141,17 +140,23 @@ const promptProject = portfolioData => {
     };
 
     promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-     const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-          if (err) throw err;
-
-          console.log('Page created! Check out index.html in this directory to see it!');
-        })
-
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
       
 
 
@@ -163,16 +168,6 @@ const promptProject = portfolioData => {
 
 
 
-
-
-
-
-
-
-
-
-
-  
 
 
 
